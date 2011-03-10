3ddcdb17a3b4477e8fa1f31e50e2eda31b6c4c75
Daniel South <djsmvp2@yahoo.com>
2011-03-09 21:05:13 -0800

    woohoo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Woohoo::Application.config.secret_token = 'a4f87d67260adf690e58d5c88b0d64f6ef06309c564e68053b3246542afe01417d819fb71cacfe9a60b0709f223061754db4e513df03d4f3dbae79253ad62f27'
